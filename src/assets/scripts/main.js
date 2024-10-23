9dee1343a4412c5ceba9c849a271b68064521b2f
JosepGual <josep.gualr@gmail.com>
2024-10-23 19:42:56 +0200

    config stylelintrc

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,12 +5,10 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
